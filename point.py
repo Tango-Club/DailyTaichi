e83067b7955d84ca0a2774a4733894a1c02f6d73
pxl290 <952130278@qq.com>
2021-12-10 00:49:26 +0800

    add point

---
+++ point.py
@@ -0,0 +1,23 @@
+import taichi as ti
+
+ti.init(arch=ti.cpu)
+
+n = 320
+pixels = ti.field(dtype=float, shape=(n, n))
+
+
+@ti.kernel
+def paint(t: float):
+    for i, j in pixels:  # Parallelized over all pixels
+        if (abs(i - n/2) ** 2)+(abs(j - n/2) ** 2) < n:
+            pixels[i, j] = 1
+        else:
+            pixels[i, j] = 0
+
+
+gui = ti.GUI("Julia Set", res=(n, n))
+
+for i in range(1000000000):
+    paint(i * 0.03)
+    gui.set_image(pixels)
+    gui.show()
